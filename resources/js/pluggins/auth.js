b312881dbe412f7567b9e8a10a4336f431a0c92d
Miguel E <escalantemiguelj@gmail.com>
2020-11-14 07:21:33 -0400

    production assets

--- resources/js/pluggins/auth.js
+++ resources/js/pluggins/auth.js
@@ -3,11 +3,9 @@ import api from './axios'
 export default {
     checkToken : async () => {
         try{
-            if (!localStorage.getItem('token')) {
-                const response = await api.get('auth/jwt')
-                if (response.status === 200 && response.data.access_token){
-                    localStorage.setItem('token',response.data.access_token);
-                }
+            const response = await api.get('auth/jwt')
+            if (response.status === 200 && response.data.access_token){
+                localStorage.setItem('token',response.data.access_token);
             }
 
         }catch (e){
